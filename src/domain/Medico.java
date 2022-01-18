e69fb0522b65632958a455a4b4b1aaab8186bb2e
Ismael Felix <felixismael72@gmail.com>
2022-01-18 15:55:16 -0300

    adding medico class and getters e setters (#4 #10)

---
+++ src/domain/Medico.java
@@ -0,0 +1,54 @@
+package src.domain;
+
+public class Medico {
+    private int id;
+    private String crm;
+    private String[] especialidade;
+    private String[] diasPlantao;
+    private float inicioExpediente;
+    private float fimExpediente;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public String[] getEspecialidade() {
+        return especialidade;
+    }
+
+    public String[] getDiasPlantao() {
+        return diasPlantao;
+    }
+
+    public float getInicioExpediente() {
+        return inicioExpediente;
+    }
+
+    public float getFimExpediente() {
+        return fimExpediente;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public void setEspecialidade(String[] especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public void setDiasPlantao(String[] diasPlantao) {
+        this.diasPlantao = diasPlantao;
+    }
+
+    public void setInicioExpediente(float inicioExpediente) {
+        this.inicioExpediente = inicioExpediente;
+    }
+
+    public void setFimExpediente(float fimExpediente) {
+        this.fimExpediente = fimExpediente;
+    }
+}
